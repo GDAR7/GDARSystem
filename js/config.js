263811dadae296158a862a4ace49807b26df6861
GDAR7 <rodriguezeibel7@gmail.com>
2026-06-29 17:09:10 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -31,11 +31,11 @@ const AREAS={
 
 // ══ USERS ══
 const USERS=[
-  {codigo:'EIBEL25',dni:'46108109',nombre:'Abel Rodriguez A.',cargo:'PCO',areas:Object.keys(AREAS)},
+  {codigo:'EIBEL25',dni:'46108109',nombre:'Abel Rodríguez A.',cargo:'PCO',areas:Object.keys(AREAS)},
   {codigo:'ECOADM',dni:'11111111',nombre:'Usuario General',cargo:'General',areas:Object.keys(AREAS)},
   {codigo:'OMARSI',dni:'87654321',nombre:'Omar Silva Santa Cruz',cargo:'Administrador',areas:['administracion']},
   {codigo:'NOEPAL',dni:'73890744',nombre:'Noelia Palomino',cargo:'Asist. Administración',areas:['administracion']},
-  {codigo:'BELCRU',dni:'74983318',nombre:'Bella E. Cruz Olivares ',cargo:'Asistenta Social',areas:['bienestarSocial']},
+  {codigo:'BELCRU',dni:'74983318',nombre:'Bella E. Cruz Olivares ',cargo:'Asistenta Social',areas:['bienestarSocial','administracion'],areaModules:{administracion:['roster']}},
   {codigo:'FLOBEN',dni:'10199407',nombre:'Flor Benites',cargo:'Jefe de Seguridad',areas:['almacenLogistica','seguridad']},
   {codigo:'ECOALM',dni:'11112222',nombre:'Logistica Central',cargo:'Ecosermo',areas:['almacenLogistica'],modules:['requerimientos','facturasPago']},
   {codigo:'ANDMAR',dni:'10199407',nombre:'Andres Martines',cargo:'Ing. Residente',areas:['administracion']},
